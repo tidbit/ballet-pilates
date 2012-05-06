cba23a69f30f2ace363ff4889ae0639dc4dfd282
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-06 18:55:53 -0500

    Add markup for header & footers

--- site/_includes/header.php
+++ site/_includes/header.php
@@ -3,7 +3,13 @@
 <header id="main-header" class="clearfix">
 <div class="wrapper">
 
-  
+  <nav id="main-nav" class="nav">
+    <h1><a href="/">Ballet & Pilates By Victoria</a></h1>
+    <ul>
+      <li id="contact"><a href="#">Contact Us</a></li>
+      <li id="signup"><a href="#">Sign Up</a></li>
+    </ul>
+  </nav>
 
 </div>
 </header> <!-- end #main_header -->
