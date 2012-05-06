ee748f5427e238851aa717ed879b50ac2ef84f64
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-06 18:36:40 -0500

    Init commit, .psds & prototype base.

---
+++ site/_includes/footer.php
@@ -0,0 +1,11 @@
+
+<footer id="main-footer" class="clearfix">
+<div class="wrapper">
+
+
+
+</div>
+</footer> <!-- end #main_footer -->
+
+</body>
+</html>
---
+++ site/_includes/header.php
@@ -0,0 +1,9 @@
+
+
+<header id="main-header" class="clearfix">
+<div class="wrapper">
+
+  
+
+</div>
+</header> <!-- end #main_header -->
