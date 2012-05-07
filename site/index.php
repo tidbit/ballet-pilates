fe1e5aa550a2031394b0003aca36a95dd72bd299
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-06 19:22:08 -0500

    Added prim & secondary cols in the index.

--- site/index.php
+++ site/index.php
@@ -2,10 +2,69 @@
 <?php include('_includes/header.php'); ?>
 
 
+<div id="home-slider">
+
+  <ul>
+    <li class="slide">
+      <a href="#">
+        <img src="style/img/kids-ballet-off.png" alt="Kids Ballet" width="320" height="530" />
+        <span>Kid's Ballet</span>
+      </a>
+    </li>
+    <li class="slide">
+      <a href="#">
+        <img src="style/img/kids-ballet-off.png" alt="Kids Ballet" width="320" height="530" />
+        <span>Pilates</span>
+      </a>
+    </li>
+    <li class="slide current">
+      <a href="#">
+        <img src="style/img/kids-ballet-off.png" alt="Kids Ballet" width="320" height="530" />
+        <span>Adult Ballet</span>
+      </a>
+    </li>
+  </ul>
+
+</div>
+
+
 <div id="main" role="main">
+<div class="wrapper">
 
-  <!-- If you build it, they will come. -->
+  <div class="prim-col">
 
+    <h1>About Victoria Simo</h1>
+    <h2>Professional Title Title</h2>
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Except Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in cucillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp.</p>
+    <p>agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo exercitation exercitation ullamco laboris nisi ut aliquip ex ea commoco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore. <a href="#">More...</a></p>
+
+  </div> <!-- End .prim-col -->
+
+
+  <div class="sec-col">
+
+    <nav class="nav">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Victoria</a></li>
+        <li><a href="#">Boutique on the Go</a></li>
+        <li><a href="#">Sign Up</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">Kid's Ballet</a></li>
+        <li><a href="#">Adult Ballet</a></li>
+        <li><a href="#">Pilates</a></li>
+      </ul>
+      <ul>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Facebook</a></li>
+      </ul>
+    </nav>
+
+  </div>
+
+
+</div>
 </div> <!-- end #main -->
 
 
