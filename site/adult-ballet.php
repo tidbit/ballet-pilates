da878fa801c44b948180c90f680e7ed3be712287
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-19 14:53:48 -0500

    Add sub page content to adult ballet, tweet some typography styles.

---
+++ site/adult-ballet.php
@@ -0,0 +1,48 @@
+<?php include('_includes/head.php'); ?>
+<?php include('_includes/header.php'); ?>
+
+
+
+
+<div id="main" role="main" class="clearfix">
+<div class="wrapper">
+
+  <div class="unify">
+    <div id="masthead">
+      <img src="style/img/masthead-placeholder.png" alt="masthead-placeholder" width="960" height="258" />
+    </div>
+  </div>
+
+  <div class="primary-col">
+
+    <div class="unify">
+      <header>
+        <h1>Adult Ballet</h1>
+        <h2><strong>Sub-title</strong></h2>
+      </header>
+  
+      <p>Who doesn’t want that long and lean dancer’s look? Ballet is the art of strength, beauty, elegance and grace. It uses all the muscles of the body to create a physical art form that incorporates beautiful music, the sheer enjoyment of movement and fitness. It challenges the body and the brain to create flow of movement.</p>
+      <p>Whether you have studied ballet for years or never stepped foot in a ballet studio, there is  a class for you. Ballet is an ideal form of exercise for all types of bodies, as one always works at their own level in order to develop fitness, strength, flexibility and artistry. Each class is a physical workout developing poise, coordination, strength, elegance and core strength. Redefine your body with Ballet! No tutu required.</p>
+      <h4>Drop-in Classes</h4>
+      <p>Drop-in dance classes are for adults and teens of all experience levels. Customize a schedule that fits your schedule.</p>
+      <h4>Sessions</h4>
+      <p>These dance classes are organized into sessions with a set number of participants and a guaranteed place in class. No matter the time of year a new workshop is starting soon. Sign up and get started today!</p>
+      <h4>Private and Duet Lessons</h4>
+      <p>Private and duet lessons are the best way to advance your technique quickly. Corrections are immediate and you will develop more confidence. Your instructor will design a personal program based on your needs and goals and they will be there just for you to motivate, train, direct, and choreograph.</p>
+    </div>
+
+  </div> <!-- End .primary-col -->
+
+
+  <div class="secondary-col">
+
+  <?php include('_includes/sidebar.php'); ?>
+
+  </div>  <!-- End .secondary-col -->
+
+
+</div>
+</div> <!-- end #main -->
+
+
+<?php include('_includes/footer.php'); ?>
