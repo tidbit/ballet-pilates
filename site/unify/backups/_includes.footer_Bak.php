b1779c74bb1677f6817568aac1a8a234fb4e797c
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-19 14:42:37 -0500

    Add contact.php - edit content with unify. Remove about since the index content is the exact same.

---
+++ site/unify/backups/_includes.footer_Bak.php
@@ -0,0 +1,28 @@
+
+<footer id="main-footer" class="clearfix">
+<div class="wrapper">
+
+  <h3 class="unify">Inspiring Creativity and Discovering a Love of<br>Movement Through Ballet and Pilates</h3>
+
+  <nav id="footer-nav" class="nav">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Victoria</a></li>
+      <li><a href="#">Sign Up Now</a></li>
+      <li><a href="#">Contact Us</a></li>
+      <li><a href="#">Facebook</a></li>
+    </ul>
+    <ul>
+      <li><a href="#">Kid's Ballet</a></li>
+      <li><a href="#">Adult Ballet</a></li>
+      <li><a href="#">Pilates</a></li>
+    </ul>
+  </nav>
+
+  <p class="unify copyright">Copright 2012 by Ballet & Pilates By Victoria. All Rights Reserved.</p>
+
+</div>
+</footer> <!-- end #main_footer -->
+
+</body>
+</html>
