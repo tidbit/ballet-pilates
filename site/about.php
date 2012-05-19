a14289cbe6cb7186f9df795e994c3250a3d9f02d
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-19 12:50:31 -0500

    Add a handful of misc pages to project.

---
+++ site/about.php
@@ -0,0 +1,43 @@
+<?php include('_includes/head.php'); ?>
+<?php include('_includes/header.php'); ?>
+
+
+
+
+<div id="main" role="main" class="clearfix">
+<div class="wrapper">
+
+  <div class="unify">
+    <div id="masthead">
+      <img src="style/img/masthead-placeholder.png" alt="masthead-placeholder" width="960" height="258" />
+    </div>
+  </div>
+
+  <div class="primary-col">
+
+    <div class="unify">
+      <header>
+        <h1>About Victoria Simo</h1>
+        <h2><strong id="internal-source-marker_0.9836212412919849"><span>Artistic Director</span></strong></h2>
+      </header>
+  
+      <p>Professional Ballerina and Instructor Victoria Simo has over 25 years of experience in the field of dance and fitness. She received her professional ballet training from Houston Ballet Academy on full scholarship, where she was awarded the prestigious Peter Schaer award. Victoria taught and choreographed at Houston Ballet Academy.</p>
+      <p>Victoria performed 9 years with Texas Ballet Theater, where she danced Principal roles including &ldquo;Alice&rdquo; in <em>Alice in Wonderland</em> and &ldquo;Snow Queen&rdquo; in <em>The Nutcracker</em>. She taught and staged several pieces at Texas Ballet Theater School. During her time as Director of Community Programs for Texas Ballet Theater, she created three new programs: Dance and Fitness Classes for Adults; a Children&rsquo;s Dance Division; and a Pilates Center.</p>
+      <p>Following the footsteps of her family, Victoria, is a certified Jazzercise Instructor. She was classically trained in Pilates at Houston Ballet and has been teaching since 2002. Her passion for dance is reflected in all her classes. She has been instructing and inspiring an array of levels and ages, ranging from budding ballerinas to coaching professionals. Her mission is to share her knowledge of the beauty and joy of the art form. Since returning to Houston, she has become well known throughout the community for her supportive, high-energy teaching style. Victoria is thrilled to realize the dream of opening her own studio, and is excited to be part of such an inspiring and life-changing program.</p>
+    </div>
+
+  </div> <!-- End .primary-col -->
+
+
+  <div class="secondary-col">
+
+  <?php include('_includes/sidebar.php'); ?>
+
+  </div>  <!-- End .secondary-col -->
+
+
+</div>
+</div> <!-- end #main -->
+
+
+<?php include('_includes/footer.php'); ?>
