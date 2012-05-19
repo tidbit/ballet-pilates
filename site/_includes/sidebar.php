2dd7342dc4b633c142c0c2d2bf127ca1133886e6
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-19 12:48:10 -0500

    Add sidebar content as an include. Add masthead placeholder image & styles.

---
+++ site/_includes/sidebar.php
@@ -0,0 +1,17 @@
+    <nav>
+      <ul class="side-nav page-nav">
+        <li><a class="current" href="#">Home</a></li>
+        <li><a href="#">About Victoria</a></li>
+        <li><a href="#">Boutique on the Go</a></li>
+        <li><a href="#">Sign Up</a></li>
+      </ul>
+      <ul class="side-nav class-nav">
+        <li><a href="#">Kid's Ballet</a></li>
+        <li><a href="#">Adult Ballet</a></li>
+        <li><a href="#">Pilates</a></li>
+      </ul>
+      <ul class="side-nav social-nav">
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Facebook</a></li>
+      </ul>
+    </nav>
