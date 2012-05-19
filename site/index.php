1126401514f1f28a536cbd16f462b815b820cccf
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-19 12:13:10 -0500

    Use unify to edit the bio on the index.php.

--- site/index.php
+++ site/index.php
@@ -32,12 +32,9 @@
 
   <div class="primary-col">
 
-    <header>
-      <h1>About Victoria Simo</h1>
-      <h2>Professional Title Title</h2>
-    </header>
-    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Except Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in cucillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp.</p>
-    <p>agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo exercitation exercitation ullamco laboris nisi ut aliquip ex ea commoco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore. <a href="#">More...</a></p>
+    <div class="unify">
+<header><h1>About Victoria Simo</h1><h2><strong id="internal-source-marker_0.9836212412919849"><span>Artistic Director</span></strong></h2></header><p>Professional Ballerina and Instructor Victoria Simo has over 25 years of experience in the field of dance and fitness. She received her professional ballet training from Houston Ballet Academy on full scholarship, where she was awarded the prestigious Peter Schaer award. Victoria taught and choreographed at Houston Ballet Academy.</p><p>Victoria performed 9 years with Texas Ballet Theater, where she danced Principal roles including &ldquo;Alice&rdquo; in <em>Alice in Wonderland</em> and &ldquo;Snow Queen&rdquo; in <em>The Nutcracker</em>. She taught and staged several pieces at Texas Ballet Theater School. During her time as Director of Community Programs for Texas Ballet Theater, she created three new programs: Dance and Fitness Classes for Adults; a Children&rsquo;s Dance Division; and a Pilates Center.</p><p>Following the footsteps of her family, Victoria, is a certified Jazzercise Instructor. She was classically trained in Pilates at Houston Ballet and has been teaching since 2002. Her passion for dance is reflected in all her classes. She has been instructing and inspiring an array of levels and ages, ranging from budding ballerinas to coaching professionals. Her mission is to share her knowledge of the beauty and joy of the art form. Since returning to Houston, she has become well known throughout the community for her supportive, high-energy teaching style. Victoria is thrilled to realize the dream of opening her own studio, and is excited to be part of such an inspiring and life-changing program.</p>
+</div>
 
   </div> <!-- End .primary-col -->
 
---
+++ site/unify/backups/index_Bak.php
@@ -0,0 +1,74 @@
+<?php include('_includes/head.php'); ?>
+<?php include('_includes/header.php'); ?>
+
+
+<div id="home-slider">
+<div class="wrapper">
+
+  <ul class="clearfix ">
+    <li id="kids" class="slide current">
+      <a href="#">
+        <span>Kid's Ballet</span>
+      </a>
+    </li>
+    <li id="pilates" class="slide">
+      <a href="#">
+        <span>Pilates</span>
+      </a>
+    </li>
+    <li id="adult" class="slide">
+      <a href="#">
+        <span>Adult Ballet</span>
+      </a>
+    </li>
+  </ul>
+
+</div>
+</div>
+
+
+<div id="main" role="main" class="clearfix">
+<div class="wrapper">
+
+  <div class="primary-col">
+
+    <div class="unify">
+      <header>
+        <h1>About Victoria Simo</h1>
+        <h2>Professional Title Title</h2>
+      </header>
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Except Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in cucillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culp.</p>
+      <p>agna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo exercitation exercitation ullamco laboris nisi ut aliquip ex ea commoco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore. <a href="#">More...</a></p>
+    </div>
+
+  </div> <!-- End .primary-col -->
+
+
+  <div class="secondary-col">
+
+    <nav>
+      <ul class="side-nav page-nav">
+        <li><a class="current" href="#">Home</a></li>
+        <li><a href="#">About Victoria</a></li>
+        <li><a href="#">Boutique on the Go</a></li>
+        <li><a href="#">Sign Up</a></li>
+      </ul>
+      <ul class="side-nav class-nav">
+        <li><a href="#">Kid's Ballet</a></li>
+        <li><a href="#">Adult Ballet</a></li>
+        <li><a href="#">Pilates</a></li>
+      </ul>
+      <ul class="side-nav social-nav">
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Facebook</a></li>
+      </ul>
+    </nav>
+
+  </div>  <!-- End .secondary-col -->
+
+
+</div>
+</div> <!-- end #main -->
+
+
+<?php include('_includes/footer.php'); ?>
