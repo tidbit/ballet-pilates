00ca23e4c50b82feb07f4e10d1d6c97985e3a98d
Matthew Simo <matthew.a.simo@gmail.com>
2012-05-21 17:31:37 -0500

    move around the sign up form.

--- contact.php
+++ contact.php
@@ -18,22 +18,6 @@
     <div class="unify">
 <header><h1>Contact Us</h1></header><p>Ballet and Pilates by Victoria is conveniently located at 15544 Ridge Park Drive, Houston, TX 77095 off Hwy 6 North. The studio is behind Carrabbas and adjacent to the Jazzercise Center.</p><p>The 3,000sq foot studio offers natural light, ballet barres, pavi dance flooring, state of the art Pilates Reformers and equipment, and a Ballet and Pilates clothing boutique. For your convenience, free childcare is available at most adult classes and sessions.</p>
 
-<!-- BEGIN: Constant Contact Bubble Opt-in Email List Form -->
-<div align="center">
-<form name="ccoptin" action="http://visitor.r20.constantcontact.com/d.jsp" target="_blank" method="post" style="margin-bottom:3;">
-<font style="font-weight: bold; font-family:Arial; font-size:18px; color:#F4B8D1;">Join Our Email List</font><br>
-<font style="font-weight: normal; font-family:Arial; font-size:10px; color:#000000;">Email:</font> <input type="text" name="ea" size="14" value="" style="font-family: Arial; font-size:10px; border:1px solid #999999;"> <input type="submit" name="go" value="Join" class="submit"  style="font-family:Arial,Helvetica,sans-serif; font-size:11px;">
-<input type="hidden" name="llr" value="nfon6tjab">
-<input type="hidden" name="m" value="1109843687597">
-<input type="hidden" name="p" value="oi">
-</form>
-</div>
-<!-- END: Constant Contact Bubble Opt-in Email List Form -->
-<!-- BEGIN: SafeSubscribe -->
-<div align="center" style="padding-top:5px;">
-<img src="https://imgssl.constantcontact.com/ui/images1/safe_subscribe_logo.gif" border="0" width="168" height="14" alt=""/>
-</div>
-<!-- END: SafeSubscribe -->
 
 <p>For more information please contact:</p>
     </div>
@@ -72,6 +56,28 @@ while(x=eval(x));}hiveware_enkoder();
 //]]>
 </script></p>
 
+
+<!-- BEGIN: Constant Contact Bubble Opt-in Email List Form -->
+<div>
+
+<form name="ccoptin" action="http://visitor.r20.constantcontact.com/d.jsp" target="_blank" method="post" style="margin-bottom:3;">
+
+<font style="font-weight: bold; font-family:Arial; font-size:24px; color:#F4B8D1;">Join Our Email List</font>
+<br>
+<font style="font-weight: normal; font-family:Arial; font-size:14px;">Email:</font>
+<input type="text" name="ea" size="14" value="" style="font-family: Arial; font-size:14px; border:1px solid #999999;">
+<input type="submit" name="go" value="Join" class="submit"  style="font-family:Arial,Helvetica,sans-serif; font-size:14px;">
+<input type="hidden" name="llr" value="nfon6tjab">
+<input type="hidden" name="m" value="1109843687597">
+<input type="hidden" name="p" value="oi">
+</form>
+</div>
+<!-- END: Constant Contact Bubble Opt-in Email List Form -->
+<!-- BEGIN: SafeSubscribe -->
+<div style="padding-top:5px;">
+<img src="https://imgssl.constantcontact.com/ui/images1/safe_subscribe_logo.gif" border="0" width="168" height="14" alt=""/>
+</div>
+<!-- END: SafeSubscribe -->
 
   </div> <!-- End .primary-col -->
 
