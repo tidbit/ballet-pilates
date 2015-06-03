1fa48df135b4c0981eafded375b1319817dc878c
Matthew Simo <matthew.a.simo@gmail.com>
2015-06-03 16:55:37 -0500

    Change order of social nav in sidebar include

--- _includes/sidebar.php
+++ _includes/sidebar.php
@@ -10,9 +10,9 @@
         <li><a <?php if (is_page('private-sessions') ) { echo 'class="current"'; } ?> href="/private-sessions/">Private Sessions</a></li>
       </ul>
       <ul class="side-nav social-nav">
+        <li><a <?php if (is_page('about') ) { echo 'class="current"'; } ?> href="/about-victoria.php">About Victoria</a></li>
         <li><a <?php if (is_page('contact') ) { echo 'class="current"'; } ?> href="/contact.php">Contact Us</a></li>
         <li><a href="https://clients.mindbodyonline.com/asp/home.asp?studioid=27108">Schedule</a></li>
         <li><a <?php if (is_page('social') ) { echo 'class="current"'; } ?> href="/social.php">Social</a></li>
-        <li><a <?php if (is_page('about') ) { echo 'class="current"'; } ?> href="/about-victoria.php">About Victoria</a></li>
       </ul>
     </nav>
