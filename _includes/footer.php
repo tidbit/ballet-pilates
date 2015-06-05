d061f23c0d9773430fef6f4eba3965862b6ac39a
Matthew Simo <matthew.a.simo@gmail.com>
2015-06-05 13:15:16 -0500

    Add footer stacked logo. Overhaul footer, change to columns based layout instead of centered mess. Add footer map, phone, email, directions link, update footer nav to reflect AI changes.

--- _includes/footer.php
+++ _includes/footer.php
@@ -6,23 +6,84 @@
 &nbsp;Find What Moves You<br>
 </h3>
 
-  <nav id="footer-nav" class="nav">
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/about-victoria.php">About&nbsp;Victoria</a></li>
-      <li><a href="https://clients.mindbodyonline.com/asp/home.asp?studioid=27108">Schedule</a></li>
-      <li><a href="/contact.php">Contact&nbsp;Us</a></li>
-      <li><a href="http://www.facebook.com/pages/Ballet-and-pilates-by-Victoria/266261843470004">Facebook</a></li>
-    </ul>
-    <ul>
-      <li><a href="/childrens-ballet.php">Children's Ballet</a></li>
-      <li><a href="/adults-ballet.php">Adult Ballet</a></li>
-      <li><a href="/pilates.php">Pilates</a></li>
-    </ul>
-  </nav>
+</div>
 
-<?php include('email-subscribe.php'); ?>
+<div class="wrapper">
+  <div class="footer-col">
 
+    <?php include('email-subscribe.php'); ?>
+
+    <p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3459.22181442122!2d-95.64194039999998!3d29.8867086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640d0cdbf20aaaf%3A0xddb25e12a6d01bcd!2s15544+Ridge+Park+Dr%2C+Houston%2C+TX+77095!5e0!3m2!1sen!2sus!4v1433523478239" width="600" height="450" frameborder="0"></iframe>
+    </p>
+
+    <h4>Questions? Get in touch!</h4>
+
+    <p>
+      <script type="text/javascript">
+      //<![CDATA[
+
+      function hiveware_enkoder(){var i,j,x,y,x=
+      "x=\"783d22793e233839346533333437343534373737343734343438343634373735343734" +
+      "3634373736343834353433373634383438343834333437343a343834353437343634333439" +
+      "34333433343437343437343234333431343734393438343334373436343734373434373534" +
+      "3637343433343334373735343734323437343a343737343438343534373737343437323437" +
+      "34333437343234373734343737343437343634383435343734323437373634373435343834" +
+      "313437343a3437373434373432343834353437343634383434343534313437343834373735" +
+      "343734323437343a3437373434333736343734343437373734373735343437373438343434" +
+      "38343634373433343737323437343634373434343834353434373534353433343734323437" +
+      "37343437373434373436343834353433343634343433343434313437343234373736343734" +
+      "35343334363434343334343431343634313437343a34373734343734323438343534373436" +
+      "34383434343637343433343334343736343734333437343234373734343737343437343634" +
+      "383435343734323437373634373435343834313437343a3437373434373432343834353437" +
+      "343634383434343534313437343834373735343734323437343a3437373434333736343734" +
+      "34343737373437373534343734343337373437343234343736343334333433343a34343733" +
+      "343434313434373333333463383a34653338333834633737376738333339373a3465343134" +
+      "63373a3464383933663764373637663738383537393463373a336334653433333a3863383a" +
+      "33633465383637663736383437343732383137363339333833363338336338393366383438" +
+      "3637333834383538333339373a33643433333a333a34633865383a233c7a3e28283c677073" +
+      "296a3e313c6a3d792f6d666f6875693c6a2c3e332a7c7a2c3e766f66746462716629282628" +
+      "2c792f747663747573296a2d332a2a3c7e7a223b793d27273b783d756e6573636170652878" +
+      "293b666f7228693d303b693c782e6c656e6774683b692b2b297b6a3d782e63686172436f64" +
+      "6541742869292d313b6966286a3c3332296a2b3d39343b792b3d537472696e672e66726f6d" +
+      "43686172436f6465286a297d79\";y='';for(i=0;i<x.length;i+=2){y+=unescape('%'" +
+      "+x.substr(i,2));}y";
+      while(x=eval(x));}hiveware_enkoder();
+
+      //]]>
+      </script>
+      <br><a href="https://goo.gl/maps/Ww4x1">Get Directions</a>
+      <br>281.855.0255
+    </p>
+
+  </div>
+
+  <div class="footer-col">
+
+    <nav id="footer-nav" class="nav">
+      <ul>
+        <li><a <?php if (is_page('kids') ) { echo 'class="current"'; } ?> href="/children-classes/">Children Classes</a></li>
+        <li><a <?php if (is_page(['adult', 'index']) ) { echo 'class="current"'; } ?> href="/adult-classes/">Adult Classes</a>
+          <ul class="sub-nav">
+            <li><a <?php if (is_page(['adult', 'ballet']) ) { echo 'class="current"'; } ?> href="/adult-classes/ballet.php">Ballet</a>
+            <li><a <?php if (is_page(['adult', 'pilates']) ) { echo 'class="current"'; } ?> href="/adult-classes/pilates.php">Pilates</a>
+          </ul>
+        </li>
+        <li><a <?php if (is_page('private-sessions') ) { echo 'class="current"'; } ?> href="/private-sessions/">Private Sessions</a></li>
+        <li><a <?php if (is_page('about') ) { echo 'class="current"'; } ?> href="/about-victoria.php">About&nbsp;Victoria</a></li>
+        <li><a <?php if (is_page('contact') ) { echo 'class="current"'; } ?> href="/contact.php">Contact&nbsp;Us</a></li>
+        <li><a href="https://clients.mindbodyonline.com/asp/home.asp?studioid=27108">Schedule</a></li>
+        <li><a <?php if (is_page('social') ) { echo 'class="current"'; } ?> href="/social.php">Social</a></li>
+      </ul>
+    </nav>
+  </div>
+
+  <div class="footer-col">
+    <a class="footer-logo" href="/"><img src="/style/img/main-logo-stacked.png" title="Ballet and Pilated By Victoria - Home"/></a>
+  </div>
+</div>
+
+<div class="wrapper">
   <p class="unify copyright">
 Copyright 2012-2015 by Ballet &amp; Pilates by Victoria. All Rights Reserved.
 </p>
