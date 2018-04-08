4b0f79a1442c266b858df0983f4ab3d6673c7156
Matthew Simo <matthew.a.simo@gmail.com>
2018-04-08 15:16:12 -0500

    Add finish markup and styling for quote component.

--- src/components/quote/index.js
+++ src/components/quote/index.js
@@ -1,6 +1,8 @@
 import { h, Component } from 'preact';
 import style from './style.scss';
 
+import headshot from '../../assets/img/victoria-headshot.jpg';
+
 export default class Quote extends Component {
 	render() {
 		return (
@@ -8,9 +10,11 @@ export default class Quote extends Component {
         <div class="wrapper">
           <div class={ style.quote }>
             <blockquote>
-              <h3>Be inspired &amp; challenged to find your best self &mdash; all while having fun. More than classes, they're experiences!</h3>
-              <span>&ndash; Victoria Simo, Owner &amp; Instructor</span>
-              <span>Headshot Image Here</span>
+              <h3 class={ style.quoteText }>
+                Be inspired &amp; challenged to find your best self &mdash; all while having fun. More than classes, they're experiences!
+              </h3>
+              <span class={ style.byLine }>&ndash; Victoria Simo, Owner &amp; Instructor</span>
+              <img class={ style.headshot } src={ headshot } />
             </blockquote>
           </div>
         </div>
