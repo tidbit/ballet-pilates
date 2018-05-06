b20bb1406442e2a0c0a38bd9e2967d64d052b8f2
Matthew Simo <matthew.a.simo@gmail.com>
2018-05-06 18:39:00 -0500

    Build out the classes overviews component and styles

--- src/components/classes-overviews/index.js
+++ src/components/classes-overviews/index.js
@@ -1,15 +1,45 @@
 import { h, Component } from 'preact';
 import style from './style.scss';
 
-export default class LoadingSpinner extends Component {
+const classes = [{
+  image: 'class-image',
+  title: 'Children Classes',
+  subtitle: 'imagine. dance. play.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu consectetur augue, id tristique eros. Praesent vel enim nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum viverra eros lorem, non eleifend nunc hendrerit non.',
+  link: '#'
+},{
+  image: 'class-image',
+  title: 'Adult Classes',
+  subtitle: 'lengthen. strengthen. love your body.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu consectetur augue, id tristique eros. Praesent vel enim nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum viverra eros lorem, non eleifend nunc hendrerit non.',
+  link: '#'
+},{
+  image: 'class-image',
+  title: 'Private Lessons',
+  subtitle: 'stretch. balance. educate.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu consectetur augue, id tristique eros. Praesent vel enim nisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum viverra eros lorem, non eleifend nunc hendrerit non.',
+  link: '#'
+}]
+
+export default class ClassesOverview extends Component {
 	render() {
 		return (
-      <div class={ style.spinner }>
-        <div class={ style.rect1 }></div>
-        <div class={ style.rect2 }></div>
-        <div class={ style.rect3 }></div>
-        <div class={ style.rect4 }></div>
-        <div class={ style.rect5 }></div>
+      <div class={`wrapper ${style.classesOverview}`}>
+        { classes.map( c => (
+          <div class={style.classOverview}>
+            <img src="https://placeimg.com/320/200/any" />
+            <div class={style.classGroup}>
+              <header>
+                <h3>{c.title}</h3>
+                <h4>{c.subtitle}</h4>
+              </header>
+              <p>{c.description}</p>
+              <footer>
+                <a href={c.link}>Learn More</a>
+              </footer>
+            </div>
+          </div>
+        ))}
       </div>
     );
 	}
