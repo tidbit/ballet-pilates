f307edd27057574755173cffee66948f867a1a9b
Matthew Simo <matthew.a.simo@gmail.com>
2021-08-31 11:48:28 -0500

    Make quote image pull from CMS

--- src/components/quote/index.js
+++ src/components/quote/index.js
@@ -1,24 +1,25 @@
-import { h, Component } from 'preact';
-import style from './style.scss';
+import { h, Component } from "preact";
+import style from "./style.scss";
 
-import headshot from '../../assets/img/victoria-headshot.jpg';
+import headshot from "../../assets/img/victoria-headshot.jpg";
 
 export default class Quote extends Component {
-	render({quote}) {
-		return (
-      <div class={ style.quoteWrapper }>
+  render({ quote, quoteImage }) {
+    const imageSrc = quoteImage && quoteImage.url ? quoteImage.url : headshot;
+    return (
+      <div class={style.quoteWrapper}>
         <div class="wrapper">
-          <div class={ style.quote }>
+          <div class={style.quote}>
             <blockquote>
-              <h3 class={ style.quoteText }>
-                {quote}
-              </h3>
-              <span class={ style.byLine }>&ndash; Victoria Simo, Owner &amp; Instructor</span>
-              <img class={ style.headshot } src={ headshot } />
+              <h3 class={style.quoteText}>{quote}</h3>
+              <span class={style.byLine}>
+                &ndash; Victoria Simo, Owner &amp; Instructor
+              </span>
+              <img class={style.headshot} src={imageSrc} />
             </blockquote>
           </div>
         </div>
       </div>
     );
-	}
+  }
 }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,85 +1,92 @@
-import { h, Component } from 'preact';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
+import { h, Component } from "preact";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
-import Sidebar from '../../components/sidebar';
-import LoadingSpinner from '../../components/loading-spinner';
-import CTABlocks from '../../components/cta-blocks';
-import Quote from '../../components/quote';
-import ClassesOverview from '../../components/classes-overviews';
-import withData from '../../components/withData';
+import Sidebar from "../../components/sidebar";
+import LoadingSpinner from "../../components/loading-spinner";
+import CTABlocks from "../../components/cta-blocks";
+import Quote from "../../components/quote";
+import ClassesOverview from "../../components/classes-overviews";
+import withData from "../../components/withData";
 
-const masthead = '/assets/img/home-masthead.jpg';
+const masthead = "/assets/img/home-masthead.jpg";
 
-import style from './style.scss';
+import style from "./style.scss";
 
 class Home extends Component {
-
   render({ page, path, data: { loading, page: Page, siteInfo, pages } }) {
-    const { homePageQuote, contentBlocks } = Object(siteInfo);
+    const { homePageQuote, homePageQuoteImage, contentBlocks } =
+      Object(siteInfo);
+    console.log(siteInfo);
 
-    return loading ? <LoadingSpinner /> : (
-      <div class={[style.home, 'home'].join(' ')}>
+    return loading ? (
+      <LoadingSpinner />
+    ) : (
+      <div class={[style.home, "home"].join(" ")}>
         <div class={`wrapper ${style.wrapper}`}>
-          {(Page && Page.masthead !== null) ? (
+          {Page && Page.masthead !== null ? (
             <div class={style.masthead}>
               <img src={Page.masthead.url} title={`${page} masthead image`} />
             </div>
           ) : (
-              <div class={style.masthead}>
-                <img src={masthead} title={`${page} masthead image`} />
-              </div>
-            )}
-
-          {contentBlocks.length > 0 && (
-            <CTABlocks blocks={contentBlocks} />
+            <div class={style.masthead}>
+              <img src={masthead} title={`${page} masthead image`} />
+            </div>
           )}
 
+          {contentBlocks.length > 0 && <CTABlocks blocks={contentBlocks} />}
         </div>
 
-        {homePageQuote && <Quote quote={homePageQuote} />}
+        {homePageQuote && (
+          <Quote quote={homePageQuote} quoteImage={homePageQuoteImage} />
+        )}
 
         <ClassesOverview classes={pages} />
 
         <Sidebar />
-
       </div>
     );
   }
 }
 
 const page = gql`
-query {
-  page(where: { page: Home }) {
-    masthead{
-      id,
-      url,
-      handle,
-      fileName
+  query {
+    page(where: { page: Home }) {
+      masthead {
+        id
+        url
+        handle
+        fileName
+      }
     }
-  },
-  siteInfo(where: { id:"cj97sbb83hzqw0128cva2wlrd" }) {
-    homePageQuote,
-    contentBlocks {
-      icon,
-      iconsize,
-      title,
-      linkLabel,
-      url
+    siteInfo(where: { id: "cj97sbb83hzqw0128cva2wlrd" }) {
+      homePageQuote
+      homePageQuoteImage {
+        id
+        fileName
+        url
+      }
+      contentBlocks {
+        icon
+        iconsize
+        title
+        linkLabel
+        url
+      }
     }
-  },
-  pages(where: {
-    page_in :[Children_Classes, Adult_Classes, Private_Sessions]
-  }) {
-    page,
-    title,
-    subtitle,
-    shortDescription,
-    previewImage {
-      url,
-      fileName
+    pages(
+      where: { page_in: [Children_Classes, Adult_Classes, Private_Sessions] }
+    ) {
+      page
+      title
+      subtitle
+      shortDescription
+      previewImage {
+        url
+        fileName
+      }
     }
-  },
-}`
+  }
+`;
 
 export default withData(graphql(page)(Home));
