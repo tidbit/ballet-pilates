d1bb6d8b4207d120dddc3463ca17b7e0a80c0197
Matthew Simo <matthew.a.simo@gmail.com>
2025-07-12 14:43:44 -0500

    2025 tanstack start init

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart({
+      pages: [
+        {
+          path: "/",
+          prerender: {
+            enabled: true,
+            outputPath: "/index.html",
+          },
+        },
+      ],
+      customViteReactPlugin: true,
+      prerender: {
+        enabled: true,
+        crawlLinks: true,
+      },
+    }),
+    viteReact(),
+  ],
+});
