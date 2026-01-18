ccd9c5798951312875f9aa158f74cbda38f731a6
Matthew Simo <matthew.a.simo@gmail.com>
2026-01-18 17:13:30 -0600

    add home carousel nav arrows for desktop

--- src/components/home-carousel.tsx
+++ src/components/home-carousel.tsx
@@ -2,6 +2,8 @@ import { Asset } from "@/gql/graphql";
 import { Carousel, CarouselItem } from "@/components/carousel";
 import { Button } from "./button";
 import { CarouselItem as CMSCarouselItem } from "@/gql/graphql";
+import { useRef, useState } from "react";
+import { useAnimationFrame } from "motion/react";
 
 function HomeCarouselItem(props: React.ComponentProps<"div">) {
   const { children } = props;
@@ -23,31 +25,110 @@ type HomeCarouselProps = {
 };
 export function HomeCarousel({ items }: HomeCarouselProps) {
   console.log({ items });
+  const [show, setShow] = useState(false);
+
   return (
     <Carousel className="w-full">
-      {items.map((item) => (
-        <CarouselItem className="w-full md:w-1/2">
-          <div
+      {items.map((item, i) => {
+        const ref = useRef<HTMLDivElement>(null);
+        const [rect, setRect] = useState<undefined | DOMRect>(undefined);
+
+        useAnimationFrame(() => {
+          const r = ref?.current?.getBoundingClientRect();
+          if (rect?.left !== r?.left) {
+            setRect(r);
+            if (!show) {
+              setShow(true);
+            }
+          }
+        });
+
+        const showPrev = show && (rect ? rect.left === 0 : false);
+        const showNext = show && (rect ? rect.left === rect.width : false);
+
+        console.log({
+          rect,
+          showPrev,
+          showNext,
+          i,
+          id: item.id,
+        });
+
+        const handleClick = () => setShow(false);
+
+        return (
+          <CarouselItem
+            id={`item-${item.id}`}
+            className="w-full md:w-1/2 relative"
             key={item.id}
-            style={{
-              backgroundImage: `url(${item.image.url})`,
-            }}
-            className="aspect-4/5 bg-cover w-full text-center flex flex-col items-center justify-end gap-4 px-16 py-12"
+            ref={ref}
           >
-            <h2 className="text-5xl leading-relaxed text-base-100 text-shadow-base-content/40 mx-auto max-w-3/5">
-              {item.title}
-            </h2>
-            <Button
-              asChild
-              className="font-sans border border-1 border-base-200/25"
-              color="primary"
-              size="sm"
+            <div
+              style={{
+                backgroundImage: `url(${item.image.url})`,
+              }}
+              className="aspect-4/5 bg-cover w-full text-center flex flex-col items-center justify-end gap-4 px-16 py-12"
             >
-              <a href={item.buttonLink}>{item.buttonLabel}</a>
-            </Button>
-          </div>
-        </CarouselItem>
-      ))}
+              <h2 className="text-5xl leading-relaxed text-base-100 text-shadow-base-content/30 text-shadow-md mx-auto max-w-3/5">
+                {item.title}
+              </h2>
+              <Button
+                asChild
+                className="font-sans border border-1 border-base-200/25"
+                color="primary"
+                size="sm"
+              >
+                <a href={item.buttonLink}>{item.buttonLabel}</a>
+              </Button>
+            </div>
+
+            {showPrev ? (
+              <PrevButton
+                href={`#item-${items[i - 1 < 0 ? items.length - 1 : i - 1].id}`}
+                onClick={() => handleClick()}
+              />
+            ) : null}
+            {showNext ? (
+              <NextButton
+                href={`#item-${items[i + 1 >= items.length ? 0 : i + 1].id}`}
+                onClick={() => handleClick()}
+              />
+            ) : null}
+          </CarouselItem>
+        );
+      })}
     </Carousel>
+  );
+}
+
+function PrevButton({ href, onClick }: { href: string; onClick: () => void }) {
+  return (
+    <div className="hidden md:flex absolute bottom-1/2 left-[40px]">
+      <Button
+        asChild
+        className="font-sans border border-1 border-base-200/25"
+        size="sm"
+        modifier="circle"
+        onClick={onClick}
+      >
+        <a href={href}>❮</a>
+      </Button>
+    </div>
+  );
+}
+
+function NextButton({ href, onClick }: { href: string; onClick: () => void }) {
+  return (
+    <div className="hidden md:flex absolute bottom-1/2 right-[40px]">
+      <Button
+        asChild
+        className="font-sans border border-1 border-base-200/25"
+        size="sm"
+        modifier="circle"
+        onClick={onClick}
+      >
+        <a href={href}>❯</a>
+      </Button>
+    </div>
   );
 }
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+import { useLayoutEffect, useCallback, useState } from "react";
 import { useParams } from "@tanstack/react-router";
 import { definedRoutes } from "@/consts";
 
@@ -16,3 +17,62 @@ export const usePage = () => {
     isAdults,
   };
 };
+
+type RectResult = {
+  bottom: number;
+  height: number;
+  left: number;
+  right: number;
+  top: number;
+  width: number;
+};
+
+export function getRect<T extends HTMLElement>(element?: T): RectResult {
+  let rect: RectResult = {
+    bottom: 0,
+    height: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    width: 0,
+  };
+  if (element) rect = element.getBoundingClientRect();
+  return rect;
+}
+
+export function useRect<T extends HTMLElement | null>(
+  ref: React.RefObject<T>,
+): RectResult {
+  const [rect, setRect] = useState<RectResult>(
+    ref && ref.current ? getRect(ref.current) : getRect(),
+  );
+
+  const handleResize = useCallback(() => {
+    if (!ref.current) return;
+    setRect(getRect(ref.current)); // Update client rect
+  }, [ref]);
+
+  useLayoutEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    handleResize();
+
+    if (typeof ResizeObserver === "function") {
+      let resizeObserver: ResizeObserver | null = new ResizeObserver(() =>
+        handleResize(),
+      );
+      resizeObserver.observe(element);
+      return () => {
+        if (!resizeObserver) return;
+        resizeObserver.disconnect();
+        resizeObserver = null;
+      };
+    } else {
+      window.addEventListener("resize", handleResize); // Browser support, remove freely
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, [ref.current]);
+
+  return rect;
+}
