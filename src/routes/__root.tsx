e2c742d0247babde8ee4a316ec41afb034d09486
Matthew Simo <matthew.a.simo@gmail.com>
2026-01-19 09:58:16 -0600

    update/fix .ico refs

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -49,19 +49,7 @@ export const Route = createRootRouteWithContext<{
         sizes: "180x180",
         href: "/apple-touch-icon.png",
       },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png",
-      },
-      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+      { rel: "manifest", href: "/manifest.json", color: "#fffff" },
       { rel: "icon", href: "/favicon.ico" },
     ],
     scripts: [
